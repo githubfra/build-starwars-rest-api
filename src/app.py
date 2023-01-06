774b02458c45f9198554d23e6b11c4ed9c9b8520
githubfra <diazcabellofrancisca@gmail.com>
2023-01-06 17:54:28 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favoritos
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,107 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
+    all_users = User.query.all()
+    usuarios = list( map ( lambda user: user.serialize(), all_users))
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "todos los users" : usuarios
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/user/favorites', methods= ['GET'])
+def get_user_favorites():
+
+    all_favorites = Favoritos.query.all()
+    favs = list( map ( lambda fav: fav.serialize(), all_favorites))
+
+    response_body = {
+        "todos los favoritos" : favs
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/people', methods= ['GET'])
+def get_people():
+    # all_people = User.query.all()
+    # print(all_people[0].__repr__())
+    all_people = People.query.all()
+    people = list( map( lambda people: people.serialize(), all_people))
+
+    return jsonify({
+        "mensaje": "todos los personajes",
+        "people": people
+    })
+
+@app.route('/planets', methods= ['GET'])
+def get_planets():
+    all_planets = Planets.query.all()
+    planets = list( map( lambda planets: planets.serialize(), all_planets))
+
+    return jsonify({
+        "mensaje": "todos los planetas",
+        "planets": planets
+    })
+
+
+@app.route('/people/<int:people_id>', methods= ['GET'])
+def get_one_people(people_id):
+    personaje = People.query.get(people_id)
+
+    return jsonify({
+        "people by id": personaje
+    })
+
+@app.route('/planets/<int:planet_id>', methods= ['GET'])
+def get_one_planet(planet_id):
+    planet = Planets.query.get(planet_id)
+    return jsonify({
+        "planet by id": planet
+    })
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods= ['POST'])
+def add_new_planet_to_current_user_favs(planet_id):
+    body = request.body(jsonify) #traer los datos del usuario. user_email
+    print(body)
+    new_favorite = Favoritos( user_email= body['email'], planet=planet_id) 
+    db.session.add(new_favorite)
+    db.session.commit()
+    return jsonify({"aqui nuevo favorito": new_favorite})
+
+@app.route('/favorite/people/<int:planet_id>', methods= ['POST'])
+def add_new_people_to_current_user_favs():
+    return jsonify({
+        "aqui ira la funcion POST para ": "Add a new favorite people to the current user with the planet id = planet_id."
+    })
+
+@app.route('/favorite/planet/<int:planet_id>', methods= ['DELETE'])
+def delete_favorite_planet(planet_id):
+
+    planet = Planets.query.get(planet_id)
+    db.session.delete(planet)
+    db.session.commit()
+
+    return jsonify({
+        "planeta borrado ": planet
+    })
+
+@app.route('/favorite/planet/<int:planet_id>', methods= ['DELETE'])
+def delete_favorite_people(planet_id):
+    del favorite.planet[planet_id]
+    return jsonify({
+        "aqui ira la funcion DELETE para ": "Delete favorite planet with the id = planet_id."
+    })
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,74 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    uid = db.Column(db.Integer, unique=True, nullable=False)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(250))
+    skin_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    birth_year = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+# representation o repr nombra a los usuarios al print, con query.all por ejemplo
+    
+    def serialize(self):
+        return {
+            "uid": self.uid,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    uid = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+# representation o repr nombra a los usuarios al print, con query.all por ejemplo
+    def serialize(self):
+        return {
+            "uid": self.uid,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "population": self.population,
+            "climate": self.climate,
+        }
+
+
+class Favoritos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(120), db.ForeignKey('user.email'))
+    people = db.Column(db.Integer, db.ForeignKey('people.id'))
+    user_fav = db.relationship('User')
+    favorite_people = db.relationship('People')  
+
+    def __repr__(self):
+        return '<Favoritos %r>' % self.id
+# representation o repr nombra a los usuarios al print, con query.all por ejemplo
+    
+    def serialize(self):
+        return {
+            "user": self.user_fav,
+            "people": self.people,
+            # do not serialize the password, its a security breach
+        }      
